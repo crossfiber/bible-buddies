7e376610fb4c6d35413c51ac7ca72f7098a09f16
Little Lights Build <build@littlelights.app>
2026-06-16 06:25:28 +0000

    Land Animals pack: 10 coloring pages (dog, cat, lion, elephant, giraffe, monkey, bear, bunny, cow, pig) cleaned + thumbed + inlined; section now live

--- build/js/manifest.js
+++ build/js/manifest.js
@@ -9,5 +9,15 @@ window.ColoringManifest = [
   { id: "color-page_joseph-coat_01", title: "Joseph's Coat", section: "bible-stories", file: "color-page_joseph-coat_01.png", thumb: "thumbs/color-page_joseph-coat_01.png" },
   { id: "color-page_nativity_01", title: "The Nativity", section: "bible-stories", file: "color-page_nativity_01.png", thumb: "thumbs/color-page_nativity_01.png" },
   { id: "color-page_noahs-ark_01", title: "Noah's Ark", section: "bible-stories", file: "color-page_noahs-ark_01.png", thumb: "thumbs/color-page_noahs-ark_01.png" },
-  { id: "color-page_palm-sunday_01", title: "Palm Sunday", section: "bible-stories", file: "color-page_palm-sunday_01.png", thumb: "thumbs/color-page_palm-sunday_01.png" }
+  { id: "color-page_palm-sunday_01", title: "Palm Sunday", section: "bible-stories", file: "color-page_palm-sunday_01.png", thumb: "thumbs/color-page_palm-sunday_01.png" },
+  { id: "color-page_dog_01", title: "Dog", section: "animals", file: "color-page_dog_01.png", thumb: "thumbs/color-page_dog_01.png" },
+  { id: "color-page_cat_01", title: "Cat", section: "animals", file: "color-page_cat_01.png", thumb: "thumbs/color-page_cat_01.png" },
+  { id: "color-page_lion_01", title: "Lion", section: "animals", file: "color-page_lion_01.png", thumb: "thumbs/color-page_lion_01.png" },
+  { id: "color-page_elephant_01", title: "Elephant", section: "animals", file: "color-page_elephant_01.png", thumb: "thumbs/color-page_elephant_01.png" },
+  { id: "color-page_giraffe_01", title: "Giraffe", section: "animals", file: "color-page_giraffe_01.png", thumb: "thumbs/color-page_giraffe_01.png" },
+  { id: "color-page_monkey_01", title: "Monkey", section: "animals", file: "color-page_monkey_01.png", thumb: "thumbs/color-page_monkey_01.png" },
+  { id: "color-page_bear_01", title: "Bear", section: "animals", file: "color-page_bear_01.png", thumb: "thumbs/color-page_bear_01.png" },
+  { id: "color-page_bunny_01", title: "Bunny", section: "animals", file: "color-page_bunny_01.png", thumb: "thumbs/color-page_bunny_01.png" },
+  { id: "color-page_cow_01", title: "Cow", section: "animals", file: "color-page_cow_01.png", thumb: "thumbs/color-page_cow_01.png" },
+  { id: "color-page_pig_01", title: "Pig", section: "animals", file: "color-page_pig_01.png", thumb: "thumbs/color-page_pig_01.png" }
 ];
